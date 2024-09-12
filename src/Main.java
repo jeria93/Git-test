6db7feaba7108ce3ee7b3dfb6f0ccfdc0f107638
Nicholas Samuelsson Jeria <nicholassamuelssonjeria@airsomtnicholas.home>
2024-09-12 19:53:43 +0200

    Added some variables

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int age = 10;
+        System.out.println(age);
+        String name = "John";
+        while (age <20) {
+            System.out.println(age);
+            age++;
+        }
+        if (name.contains("John")) {
+            System.out.println("John is here");
+        }
+    }
+}
